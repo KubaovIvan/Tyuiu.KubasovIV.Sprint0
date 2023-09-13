0aae40dac3ec354f6780306bfd252bbbe08c9d49
Tyuiu.KubasovIV@outlook.com <ubasovvanya@gmail.com>
2023-09-13 19:17:05 +0500

    Task4

---
+++ Tyuiu.KubasovIV.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KubasovIV.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        //Пример линейной структуры (Метод сложения)
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Пример линейной структуры (Метод вычетания)
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Пример линейной структуры (Метод умножения)
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        //Пример линейной структуры (Метод деления)
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.KubasovIV.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KubasovIV.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KubasovIV.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8dc8d20-8ef9-4e83-bcaa-af6a8d4f6b90")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
